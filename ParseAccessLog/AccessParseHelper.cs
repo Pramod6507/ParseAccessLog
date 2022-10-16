4c4b1071e277e7db3b6c1508e030295c28485328
Pramod Kumar <pramod6507@gmail.com>
2022-10-16 12:16:57 -0600

    Add project files.

---
+++ ParseAccessLog/AccessParseHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace ParseAccessLog
+{
+    public static class AccessParseHelper
+    {
+        public static string IpAddressLabel(string ipAddress)
+                => string.Join(".", ipAddress.Split('.').Select(part => part.PadLeft(3, '0')));
+
+
+    }
+}
---
+++ ParseAccessLog/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ParseAccessLog
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] text = File.ReadAllLines(@"E:\Work\Valtech\access.log", Encoding.UTF8);
+
+            List<string> clientIPs = new List<string>();
+            var accessCount = new List<KeyValuePair<int, string>>();
+
+            foreach (var line in text)
+            {
+                if (line[0] != '#')
+                {
+                    string[] words = line.Split(' ');
+                    if (words[7] == "80" && words[8] == "GET" && !words[2].StartsWith("207.114"))
+                    {
+                        clientIPs.Add(words[2]);
+                    }
+                    
+                }
+                
+            }
+            
+            var sortedClientIPs = clientIPs.OrderByDescending(ip => AccessParseHelper.IpAddressLabel(ip));
+            List<string> uniqueIPs = sortedClientIPs.Distinct().ToList();
+            foreach (var IP in uniqueIPs)
+            {
+                int count = sortedClientIPs.Where(x => x.Equals(IP)).Count();
+                //Console.WriteLine(string.Join(", ",count, IP ));
+                accessCount.Add(new KeyValuePair<int, string>(count, IP));
+
+            }
+            
+            var sortedAccessCount = accessCount.OrderByDescending(c => c.Key).ToList();
+            var arrayList = new ArrayList();
+
+            foreach (var countPair in sortedAccessCount)
+            {
+                List<string> ipOctets = countPair.Value.Split('.').ToList();
+                int[] arr = { countPair.Key, int.Parse(ipOctets[0]), int.Parse(ipOctets[1]), int.Parse(ipOctets[2]), int.Parse(ipOctets[3]) };
+                arrayList.Add(arr);
+                Console.WriteLine(string.Join(", ", countPair.Key, countPair.Value));
+            }
+
+            
+            Console.WriteLine("End");
+        }
+
+
+    }
+}
