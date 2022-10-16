44e8c8eb15d5dda9bf08044902d05667f4d906f8
Pramod Kumar <pramod6507@gmail.com>
2022-10-16 13:12:35 -0600

    Refactored code

--- ParseAccessLog/AccessParseHelper.cs
+++ ParseAccessLog/AccessParseHelper.cs
@@ -1,4 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace ParseAccessLog
 {
@@ -7,6 +11,85 @@ namespace ParseAccessLog
         public static string IpAddressLabel(string ipAddress)
                 => string.Join(".", ipAddress.Split('.').Select(part => part.PadLeft(3, '0')));
 
+        public static string[] ReadAccessLog(string location)
+        {
+            return File.ReadAllLines(location, Encoding.UTF8);
+        }
+
+        public static List<string> ExtractClientIPs(string[] text)
+        {
+            List<string> clientIPs = new List<string>();
+            foreach (var line in text)
+            {
+                if (line[0] != '#')
+                {
+                    string[] words = line.Split(' ');
+                    if (words[7] == "80" && words[8] == "GET" && !words[2].StartsWith("207.114"))
+                    {
+                        clientIPs.Add(words[2]);
+                    }
+
+                }
+
+            }
+
+            return clientIPs;
+        }
+
+
+
+        public static List<KeyValuePair<int, string>> CountClientAccess(IOrderedEnumerable<string> sortedClientIPs, List<string> uniqueIPs)
+        {
+            var accessCount = new List<KeyValuePair<int, string>>();
+            foreach (var IP in uniqueIPs)
+            {
+                int count = sortedClientIPs.Where(x => x.Equals(IP)).Count();
+                Console.WriteLine(string.Join(", ", count, IP));
+                accessCount.Add(new KeyValuePair<int, string>(count, IP));
+
+            }
+
+            return accessCount;
+        }
+
+        public static IOrderedEnumerable<string> SortIPsByAddress(List<string> clientIPs)
+        {
+            return clientIPs.OrderByDescending(ip => AccessParseHelper.IpAddressLabel(ip));
+        }
+
+        public static List<string> ExtractDistinctIPs(IOrderedEnumerable<string> sortedClientIPs)
+        {
+            return sortedClientIPs.Distinct().ToList();
+        }
+
+        public static List<KeyValuePair<int, string>> SortIPsByNoOfAccess(List<KeyValuePair<int, string>> accessCount)
+        {
+            return accessCount.OrderByDescending(c => c.Key).ToList();
+        }
+
+
+        public static void WriteReportToFile(List<KeyValuePair<int, string>> sortedAccessCount, string location)
+        {
+            FileInfo fileInfo = new FileInfo(location);
+
+            if (fileInfo.Exists)
+            {
+                fileInfo.Delete();
+            }
+
+            using (StreamWriter sw = fileInfo.CreateText())
+            {
+                sw.WriteLine("New file created: {0}", DateTime.Now.ToString());
+                sw.WriteLine("===============================================================================================================");
+                sw.WriteLine("Final IP List");
+                sw.WriteLine("===============================================================================================================");
+                foreach (var countPair in sortedAccessCount)
+                {
+                    sw.WriteLine(string.Join(", ", countPair.Key, countPair.Value));
+                }
+                sw.WriteLine("End of file");
+            }
+        }
 
     }
 }
--- ParseAccessLog/Program.cs
+++ ParseAccessLog/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
+
 
 namespace ParseAccessLog
 {
@@ -12,49 +11,40 @@ namespace ParseAccessLog
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            string[] text = File.ReadAllLines(@"E:\Work\Valtech\access.log", Encoding.UTF8);
-
-            List<string> clientIPs = new List<string>();
-            var accessCount = new List<KeyValuePair<int, string>>();
-
-            foreach (var line in text)
+            try
             {
-                if (line[0] != '#')
+
+
+                Console.WriteLine("Hello World!");
+                string[] text = AccessParseHelper.ReadAccessLog(@"E:\Work\Valtech\access.log");
+
+                List<string> clientIPs = AccessParseHelper.ExtractClientIPs(text);
+                IOrderedEnumerable<string> sortedClientIPs = AccessParseHelper.SortIPsByAddress(clientIPs);
+                List<string> uniqueIPs = AccessParseHelper.ExtractDistinctIPs(sortedClientIPs);
+
+                List<KeyValuePair<int, string>> accessCount = AccessParseHelper.CountClientAccess(sortedClientIPs, uniqueIPs);
+                List<KeyValuePair<int, string>> sortedAccessCount = AccessParseHelper.SortIPsByNoOfAccess(accessCount);
+
+                AccessParseHelper.WriteReportToFile(sortedAccessCount, @"E:\Work\Valtech\report.txt");
+
+                Console.WriteLine("New file created: {0}", DateTime.Now.ToString());
+                Console.WriteLine("=======================================================================");
+                Console.WriteLine("Final List");
+                Console.WriteLine("=======================================================================");
+
+                foreach (var countPair in sortedAccessCount)
                 {
-                    string[] words = line.Split(' ');
-                    if (words[7] == "80" && words[8] == "GET" && !words[2].StartsWith("207.114"))
-                    {
-                        clientIPs.Add(words[2]);
-                    }
-                    
+                    Console.WriteLine(string.Join(", ", countPair.Key, countPair.Value));
                 }
-                
+
+
+                Console.WriteLine("End");
+
             }
-            
-            var sortedClientIPs = clientIPs.OrderByDescending(ip => AccessParseHelper.IpAddressLabel(ip));
-            List<string> uniqueIPs = sortedClientIPs.Distinct().ToList();
-            foreach (var IP in uniqueIPs)
+            catch (Exception Ex)
             {
-                int count = sortedClientIPs.Where(x => x.Equals(IP)).Count();
-                //Console.WriteLine(string.Join(", ",count, IP ));
-                accessCount.Add(new KeyValuePair<int, string>(count, IP));
-
+                Console.WriteLine(Ex.ToString());                
             }
-            
-            var sortedAccessCount = accessCount.OrderByDescending(c => c.Key).ToList();
-            var arrayList = new ArrayList();
-
-            foreach (var countPair in sortedAccessCount)
-            {
-                List<string> ipOctets = countPair.Value.Split('.').ToList();
-                int[] arr = { countPair.Key, int.Parse(ipOctets[0]), int.Parse(ipOctets[1]), int.Parse(ipOctets[2]), int.Parse(ipOctets[3]) };
-                arrayList.Add(arr);
-                Console.WriteLine(string.Join(", ", countPair.Key, countPair.Value));
-            }
-
-            
-            Console.WriteLine("End");
         }
 
 
